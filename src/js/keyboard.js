8c4ce32585340bc695993b45b3df79dff4ee2e1e
Hinell <al.neodim@gmail.com>
2017-10-08 23:09:12 +0300

    ALt & shit key support

--- src/js/keyboard.js
+++ src/js/keyboard.js
@@ -1,7 +1,12 @@
 w.down = {};
 onkeydown = e => {
     w.down[e.keyCode] = true;
+    w.down.alt      = e.altKey
+    w.down.shift    = e.altKey
+    
 };
 onkeyup = e => {
     w.down[e.keyCode] = false;
+    w.down.alt      = e.altKey
+    w.down.shift    = e.altKey
 };
